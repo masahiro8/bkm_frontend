670c8956c9fb324df74b709b80eb613849765722
masahiro8 <masahiro819@gmail.com>
2022-09-06 04:40:09 +0900

    1 file modified, 1 added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import breaks from 'remark-breaks';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [breaks],
+  },
 });
---
+++ src/pages/blog/markdown.md
@@ -0,0 +1,11 @@
+---
+layout: "../../layouts/BlogPost.astro"
+title: "Markdown"
+description: "Lorem ipsum dolor sit amet"
+pubDate: "Jul 08 2022"
+heroImage: "/placeholder-hero.jpg"
+---
+
+⇦ 全角スペース
+⇦ 半角スペース
+⇦ なし
